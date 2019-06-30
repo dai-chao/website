fd00d5200c0079c8eb94ce14dc5edded3f25c24c
smallItachi <1113970593@qq.com>
2019-06-30 15:21:32 +0800

    删除没用的类名

--- src/components/about/index.js
+++ src/components/about/index.js
@@ -19,9 +19,9 @@ class About extends Component{
         return(
             <div className="about">
                 <Link to="/about">
-                    <h2 className="fl wow bounceIn">- 关于我们 -</h2>
-                    <img src={aboutimg} alt="" className="fl wow slideInUp"/>
-                    <h4 className="fl wow slideInUp">中汇祥龄电子商务(北京)有限公（ZHXL）成立于2019年，由中付航天科技创新数据中心（深圳）股份有限公司旗下投资经营，集团在中国老年人才信息中心，中国老龄协会组织指导下，依托中国老龄学会所属北京国龄智慧健康养老产业发展中心共同搭建互联网+养老生活服务平台，旨在分担国家人口老龄化压力，减轻社会养老负担，贯彻国家新时代养老政策。</h4>
+                    <h2 className=" wow bounceIn">- 关于我们 -</h2>
+                    <img src={aboutimg} alt="" className=" wow slideInUp"/>
+                    <h4 className=" wow slideInUp">中汇祥龄电子商务(北京)有限公（ZHXL）成立于2019年，由中付航天科技创新数据中心（深圳）股份有限公司旗下投资经营，集团在中国老年人才信息中心，中国老龄协会组织指导下，依托中国老龄学会所属北京国龄智慧健康养老产业发展中心共同搭建互联网+养老生活服务平台，旨在分担国家人口老龄化压力，减轻社会养老负担，贯彻国家新时代养老政策。</h4>
                 </Link>
             </div>
         )
--- src/pages/contact/index.js
+++ src/pages/contact/index.js
@@ -25,15 +25,15 @@ class Contact extends Component{
                 {this.props.children}
                 <Header/>
                 <Banner/>
-                <p className=" fl wow slideInRight"> - 联系我们 -</p>
-                <ul className=" fl wow slideInLeft">
+                <p className="  wow slideInRight"> - 联系我们 -</p>
+                <ul className="  wow slideInLeft">
                     <li>电话：010-68570339</li>
                     <li>地址：北京市 丰台区 汉威国际广场 三区5栋</li>
                     <li>邮箱：zhxl_yl@163.com</li>
                 </ul>
-                <img src={e1} alt="" className=" fl wow slideInRight"/>
-                <h3 className=" fl wow slideInRight">关注订阅号“祥龄资讯</h3>
-                <h4 className=" fl wow slideInRight">获取更多资讯</h4>
+                <img src={e1} alt="" className="  wow slideInRight"/>
+                <h3 className="  wow slideInRight">关注订阅号“祥龄资讯</h3>
+                <h4 className="  wow slideInRight">获取更多资讯</h4>
                 <Footer/>
             </div>
         )
