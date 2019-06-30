b12be6dabf92da2eb9a8daad1b4babc575013955
smallItachi <1113970593@qq.com>
2019-06-30 11:49:18 +0800

    修改wow.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import "./index.css"
+import './css/animate.min.css'
 import Router from "./router/index"
 import * as serviceWorker from './serviceWorker';
+
 
 ReactDOM.render(<Router />, document.getElementById('root'));
 
