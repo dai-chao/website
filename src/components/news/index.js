da471bd93cc4b535121feef8d5c276cb9678276c
dai-chao <m17812040809@163.com>
2019-07-02 18:21:26 +0800

    修改新闻详情页样式

--- src/components/news/index.js
+++ src/components/news/index.js
@@ -31,29 +31,28 @@ import n4 from "../../images/主页/news_4.jpg"
 
                     <h2 className="  wow bounceIn"> - 新闻动态 -</h2>
                     <ul>
-                        <a href="https://mp.weixin.qq.com/s/w3Yg6t-sE8P1ZRBH8H850Q">
+                        <a href="javascript:;">
                         <li className="  wow slideInUp">
                             <img src={n2} alt=""/>
                             <h2>中汇祥龄协同老龄办筹划明星进社区宣导新时代智慧健康养老服务平台。</h2>
                             <h4>2019-6-29</h4>
                         </li>
                         </a>
-                        <a href="https://mp.weixin.qq.com/s/8wPPOJgBRqWys6r5d3X0zA">
+                        <a href="javascript:;">
                         <li className="  wow slideInUp">
                             <img src={n3} alt=""/>
                             <h2>银河保险·国龄·中汇祥龄三方正式签约成战略合作伙伴。</h2>
                             <h4>2019-6-29</h4>
                         </li>
                         </a>
-                        <a href="https://mp.weixin.qq.com/s/EFOxA44XKqXeUnqNuXjePQ">
-                        
+                        <a href="javascript:;">
                         <li className="  wow slideInUp">
                             <img src={n1} alt="" className="n2"/>
                             <h2>中国老龄协会领导莅临中汇祥龄公司考察调研。</h2>
                             <h4>2019-6-29</h4>
                         </li>
                         </a>
-                        <a href="https://mp.weixin.qq.com/s/VGesHZ2rADdNx964FMK1hw">
+                        <a href="javascript:;">
                         <li className="  wow slideInUp">
                             <img src={n4} alt=""/>
                             <h2>北京国龄中心与中航在京签约共建这样一平台。</h2>
